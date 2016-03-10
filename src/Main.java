55230022a220cabf7cb002335bd5f46bed81ea25
michael <darekmichael@gmail.com>
2016-03-10 11:58:40 -0700

    Server displays IP address when started.  In real world application this would be a static address

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.net.InetAddress;
 import java.util.ArrayList;
 
 /**
@@ -7,13 +8,14 @@ public class Main {
     public static void main(String[] args)
     {
         ArrayList<Tracker> trackerList = new ArrayList<>();
+        String ipAddress = getIPaddress();
 
         MultiThreadedServer server = new MultiThreadedServer(9000, trackerList);
         new Thread(server).start();
 
         try {
             Thread.sleep(1000);
-            System.out.println("Redirect Server Running...");
+            System.out.println("Redirect Server Running at: " + ipAddress);
             while(true);
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -21,4 +23,17 @@ public class Main {
         System.out.println("Stopping Server");
         server.stop();
     }
+
+    public static String getIPaddress(){
+        String IPaddress = "";
+        try {
+            InetAddress thisIp = InetAddress.getLocalHost();
+            IPaddress = thisIp.getHostAddress();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        return IPaddress;
+    }
+
 }
